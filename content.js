d60450db08cd7d25f0ecca634645de55837d7675
Yaroslav Luzin <jardev@gmail.com>
2021-08-03 12:07:42 +0300

    Removed unnecessary code

--- content.js
+++ content.js
@@ -35,14 +35,3 @@ function replaceText(element) {
     }
   }
 }
-
-chrome.runtime.onMessage.addListener(
-  function (request, sender, sendResponse) {
-    if (request.message === "clicked_browser_action") {
-      alert("receive message");
-      var firstHref = $("a[href^='http']").eq(0).attr("href");
-
-      console.log(firstHref);
-    }
-  }
-);
