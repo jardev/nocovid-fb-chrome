28cc11a840ad1ee807f030b8f2e6a26a059d6a4f
Yaroslav Luzin <jardev@gmail.com>
2021-08-04 21:14:09 +0300

    Fixed cursor jumping when entering text into a reply

--- content.js
+++ content.js
@@ -20,8 +20,12 @@ function replaceText(element) {
       element.childNodes.forEach(replaceText);
     }
   } else if (element.nodeType === Text.TEXT_NODE) {
-    element.textContent = element.textContent.replace(/covid/gi, "■■■■");
-    element.textContent = element.textContent.replace(/-19/gi, "■■■■");
+    if (element.textContent.match(/covid/gi)) {
+      element.textContent = element.textContent.replace(/covid/gi, "■■■■");
+    }
+    if (element.textContent.match(/-19/gi)) {
+      element.textContent = element.textContent.replace(/-19/gi, "■■■■");
+    }
 
     if (element.textContent.match("Visit the .* Information Center") &&
         element.parentElement &&
